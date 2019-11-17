748b11ba456b9badad91821abee953a5d0b89c12
zhangjie <1450443957@qq.com>
2019-11-17 20:58:01 +0800

    master 将password 改成passwor

--- src/com/cupdata/controller/VueController.java
+++ src/com/cupdata/controller/VueController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("vue")
 public class VueController {
 	@RequestMapping("find")
-      public String find(String name,String password) {
+      public String find(String name,String passwor) {
 		return "你好!"+name;
     	  
       }
