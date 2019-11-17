2b7f4b42c5e197706c695290be28891a68d52bb7
zhangjie <1450443957@qq.com>
2019-11-17 21:34:07 +0800

    name +1

--- src/com/cupdata/controller/VueController.java
+++ src/com/cupdata/controller/VueController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("vue")
 public class VueController {
 	@RequestMapping("find")
-      public String find(String name,String password) {
-		return "你好!"+name;  
+      public String find(String name1,String password) {
+		return "你好!"+name1;  
       }
 }
