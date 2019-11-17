89e6637774c682fff3b972ceac2c54f46b6bf22a
zhangjie <1450443957@qq.com>
2019-11-17 20:53:14 +0800

    hot_fix的提交了你好+name

--- src/com/cupdata/controller/VueController.java
+++ src/com/cupdata/controller/VueController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("vue")
 public class VueController {
 	@RequestMapping("find")
-      public String find() {
-		return "11";
+      public String find(String name,String password) {
+		return "你好!"+name;
     	  
       }
 }
