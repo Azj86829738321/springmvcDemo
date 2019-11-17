670c63e80e2c2a3375a67cbefab663dd3ab36734
zhangjie <1450443957@qq.com>
2019-11-17 22:44:05 +0800

    password ->1

--- src/com/cupdata/controller/VueController.java
+++ src/com/cupdata/controller/VueController.java
@@ -9,7 +9,7 @@ public class VueController {
 	@RequestMapping("find")
 
 
-      public String find(String name,String password2) {
+      public String find(String name,String password1) {
 		return "你好!"+name;  
 
     
